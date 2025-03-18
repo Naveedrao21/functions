e923e2fd7410f94958fac36479c85782f4a6a228
Rao Naveed <oyerao18@gmail.com>
2025-03-18 17:42:17 +0500

    add print_labeled_measurement function and call it in main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     println!("Main function.");
     another_function(5);
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function(x: u32) {
     println!("The value of x is : {x}");
 }
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
