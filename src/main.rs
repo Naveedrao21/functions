a2f75650b8ef0b384d8be7b86b995d8156b1cab5
Rao Naveed <oyerao18@gmail.com>
2025-03-20 15:41:01 +0500

    add nine function and update main to demonstrate its usage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ fn main() {
     println!("Main function.");
     another_function(5);
     print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 4;
+        x + 3
+    };
+    println!("The value of y is: {y}");
+    let c = nine();
+    println!("The value of c is: {c}");
 }
 
 fn another_function(x: u32) {
@@ -10,4 +18,6 @@ fn another_function(x: u32) {
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 }
-
+fn nine() -> i32 {
+    9
+}
